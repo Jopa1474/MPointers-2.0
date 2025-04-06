5b716b50c5e13942def4ea2bfcfae54881710e36
YitzyJimenezJ <yitzyjimenezj@gmail.com>
2025-04-05 20:56:54 -0600

    Prototipo Memory Manager

---
+++ memory_manager/main.cpp
@@ -0,0 +1,8 @@
+// memory_manager/main.cpp
+
+#include <iostream>
+
+int main() {
+    std::cout << "Memory Manager funcionando correctamente (compilación OK)" << std::endl;
+    return 0;
+}
--- src/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "MPointers-2.0 Iniciado" << std::endl;
-    return 0;
-}
-
